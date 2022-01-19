abd6959974956555c6a0b19b2a56238e3b5523a2
Giovani Trevisol <giovanitrevisol93@gmail.com>
2022-01-19 13:13:00 -0300

    compilacao manual

---
+++ compilacao-manual/OlaDev.java
@@ -0,0 +1,13 @@
+public class OlaDev{
+    public static void main (String[] args){
+        System.out.println("Executando compilação manual!!!");
+    }
+}
+
+//nome do arquivo deve ser exatamente igual ao nome da classe.java
+//abrir terminal na pasta que salvou o arquivo e rodar javac nomeArquivo
+//javac OlaDev
+//sera criado o arquivo nomeArquivo.class
+
+//Para rodar o arquivo, ainda no terminal, use:
+// java nomeArquivo   -> lembrando que não devemos colocar a extenção
